5e0f59813dfa8671a5edc3af6763456ad1e16c35
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-01-22 22:54:37 -0800

    updated margin on content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -61,7 +61,7 @@ export default function Content() {
       </div>
 
       {/* Three */}
-      <div className="mx-auto max-w-md px-4 lg:mb-96 md:mb-40 mb-64 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+      <div className="mx-auto max-w-md px-4 lg:mb-96 md:mb-40 mb-20 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
         <div>
           <h2 className="text-base font-semibold tracking-wider text-sky-800 uppercase">Three</h2>
           <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
